315523fdbdba3447aa798a644e8b2d4e4b79bd8d
Marcopq270 <marco.balderaspq@gmail.com>
2019-09-17 09:13:47 -0500

    First Commit

--- appclases/TableViewController.swift
+++ appclases/TableViewController.swift
@@ -9,38 +9,50 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    
+    var productos = [producto]()
+    @IBOutlet var Tabla: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+      Tabla.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return productos.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)as! TableViewCell
+        let prod: producto
+        prod=productos[indexPath.row]
+        cell.lbltitulo.text=prod.nomProducto
+        cell.lblsubtitulo.text=prod.existencia
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       let prod=productos[indexPath.row]
+        print("Diste click en"+prod.nomProducto)
+    }
+    
+
+
+}
+
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -87,4 +99,4 @@ class TableViewController: UITableViewController {
     }
     */
 
-}
+
--- appclases/ViewController.swift
+++ appclases/ViewController.swift
@@ -9,19 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var productos = [producto]()
+    
     @IBOutlet weak var txtexisten: UITextField!
     @IBOutlet weak var txtnom: UITextField!
     @IBOutlet weak var txtclave: UITextField!
     @IBAction func btnagregar(_ sender: UIButton) {
+        
+        let clave =
+            txtclave.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let nom =
+            txtnom.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let existe =
+            txtexisten.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if(clave?.isEmpty)!{
+            showAlerta(titulo: "Error", mensaje: "Caja vacia")
+            txtclave.becomeFirstResponder()
+            return
+        }
+        
+        if(nom?.isEmpty)!{
+            showAlerta(titulo: "Error", mensaje: "Caja vacia")
+            txtnom.becomeFirstResponder()
+            return
+        }
+        
+        if(existe?.isEmpty)!{
+            showAlerta(titulo: "Error", mensaje: "Caja vacia")
+            txtexisten.becomeFirstResponder()
+            return
+        }
+        productos.append(producto(idprod: clave!, nomProd: nom!, existen: existe!))
+        txtclave.text=""
+        txtnom.text=""
+        txtexisten.text="0"
+        txtclave.becomeFirstResponder()
     }
+    
     @IBAction func btnconsultar(_ sender: UIButton) {
+        
     }
+    
+    func showAlerta(titulo: String, mensaje: String){
+        //Crea La Alerta
+        let alert = UIAlertController(title: titulo, message: mensaje, preferredStyle: UIAlertController.Style.alert)
+        //Agregar Un Boton
+        alert.addAction(UIAlertAction(title: "Aceptar", style: UIAlertAction.Style.default, handler: nil))
+        //Muestra El Alerta
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier=="push"){
+            let tablesegue=segue.destination as! TableViewController
+            tablesegue.productos=productos
+        }
+    }
 
 }
 
